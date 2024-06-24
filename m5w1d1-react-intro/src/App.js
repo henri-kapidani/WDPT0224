bb97c007d1ad14441a6bbb72bb7f5370392bd768
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2024-06-24 22:16:51 +0200

    m1w1d1 react intro

---
+++ m5w1d1-react-intro/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <Header />
+            <Header />
+            <Header />
+            <Header />
+            <h1>Ciao a tutti</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ m5w1d1-react-intro/src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div>
+            <h1>Sono l'header</h1>
+            <button>ciao</button>
+        </div>
+    );
+}
+
+export default Header;
