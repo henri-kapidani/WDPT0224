2b75b27a6ec7099e9e28ae1414e6b54a73bc218b
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2024-07-04 20:31:47 +0200

    commit

--- m5w2d2-remote-fetches/project-01/src/App.jsx
+++ m5w2d2-remote-fetches/project-01/src/App.jsx
@@ -2,8 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import NavTop from './components/NavTop';
+// import MainButton from './components/MainButton';
 import MainUseEffect from './components/MainUseEffect';
-import MainButton from './components/MainButton';
 // import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 
@@ -14,8 +14,8 @@ function App() {
             <Container>
                 <Row>
                     <Col xs={12} md={12}>
-                        <MainButton />
-                        {/* <MainUseEffect /> */}
+                        {/* <MainButton /> */}
+                        <MainUseEffect />
                     </Col>
                     {/* <Col xs={12} md={3}>
                         <Sidebar />
