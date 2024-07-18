be22f5a9c769bee79cb59b79bc755b04d846c89a
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2024-07-18 22:12:41 +0200

    removed project 02

--- m5w4d2-testing/project-02/src/components/Sidebar.jsx
+++
@@ -1,7 +0,0 @@
-function Sidebar() {
-    return (
-        <aside className="h-100 py-5 bg-secondary text-center">Sidebar</aside>
-    );
-}
-
-export default Sidebar;
--- m5w4d2-testing/project-02/src/pages/Contacts.jsx
+++
@@ -1,9 +0,0 @@
-function Contacts() {
-    return (
-        <div>
-            <h1>Contacts</h1>
-        </div>
-    );
-}
-
-export default Contacts;
