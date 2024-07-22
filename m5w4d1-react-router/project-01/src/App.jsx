66c773778bf29bca664506af3d6db01da5b94112
henri-kapidani <97576915+henri-kapidani@users.noreply.github.com>
2024-07-22 19:30:16 +0200

    add env

--- m5w4d1-react-router/project-01/src/App.jsx
+++ m5w4d1-react-router/project-01/src/App.jsx
@@ -16,40 +16,49 @@ import NotFound from './pages/NotFound';
 
 function App() {
     return (
-        <BrowserRouter>
-            {/* <HeaderSimple /> */}
-            <HeaderBootstrap />
-            <Container>
-                <Row>
-                    <Col xs={12} md={10}>
-                        {/* Parte che differisce tra le varie pagine */}
-                        <Routes>
-                            {/* Verrà renderizzato uno solo di questi componenti */}
-                            {/* in base all'indirizzo in cui ci troviamo */}
-                            <Route path="/" element={<Home />} />
-                            <Route path="/about" element={<About />} />
-                            <Route path="/contacts" element={<Contacts />} />
-                            <Route path="/posts" element={<PostList />} />
+        <>
+            {process.env.REACT_APP_API_STIVE_LINKEDIN}
+            <BrowserRouter>
+                {/* <HeaderSimple /> */}
+                <HeaderBootstrap />
+                <Container>
+                    <Row>
+                        <Col xs={12} md={10}>
+                            {/* Parte che differisce tra le varie pagine */}
+                            <Routes>
+                                {/* Verrà renderizzato uno solo di questi componenti */}
+                                {/* in base all'indirizzo in cui ci troviamo */}
+                                <Route path="/" element={<Home />} />
+                                <Route path="/about" element={<About />} />
+                                <Route
+                                    path="/contacts"
+                                    element={<Contacts />}
+                                />
+                                <Route path="/posts" element={<PostList />} />
 
-                            <Route path="/posts/:id" element={<Post />} />
-                            {/* esempi di indirizzi che vengono catturati da questa Route:s
+                                <Route path="/posts/:id" element={<Post />} />
+                                {/* esempi di indirizzi che vengono catturati da questa Route:s
                                 /posts/2
                                 /posts/10
                                 /posts/ciao
                             */}
 
-                            <Route path="/404" element={<NotFound />} />
-                            <Route path="/*" element={<Navigate to="/404" />} />
-                        </Routes>
-                    </Col>
-                    <Col xs={12} md={2}>
-                        <Sidebar />
-                    </Col>
-                </Row>
-            </Container>
+                                <Route path="/404" element={<NotFound />} />
+                                <Route
+                                    path="/*"
+                                    element={<Navigate to="/404" />}
+                                />
+                            </Routes>
+                        </Col>
+                        <Col xs={12} md={2}>
+                            <Sidebar />
+                        </Col>
+                    </Row>
+                </Container>
 
-            <Footer />
-        </BrowserRouter>
+                <Footer />
+            </BrowserRouter>
+        </>
     );
 }
 
